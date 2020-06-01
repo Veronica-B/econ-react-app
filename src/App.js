0c33d8e6a44ddc2cbc09cadc37312335a31d0690
Veronica Barcia <veronicabarcia4@gmail.com>
2020-06-01 11:45:58 -0400

    netify stop this plz

--- src/App.js
+++ src/App.js
@@ -7,20 +7,35 @@ import ContactPage from './pages/contact';
 import './css/general.css'
 import './css/contact.css'
 
+import ProductCard from './components/product.js';
+import Navbar from './components/navbar.js';
+import Footer from './components/footer.js';
+
+
+
 class App extends React.Component {
 
 
   render(){
   return (
-    <Router >
-      <Switch>
-        {/* Switch is needed because i have mutiple routes */}
-     <Route exact path="/" component={ProductsPage} />
-     <Route exact path="/index.html" component={IndexPage} />
-     <Route exact path="/products.html" component={ProductsPage} />
-     <Route exact path="/contact.html" component={ContactPage} />
-     </Switch>
-    </Router>
+    <div>
+    <Navbar />
+    <div className="background-color">
+         <div className="background_color grid-borders">
+      <ProductCard />
+      </div>
+    </div>
+    <Footer />
+    </div>
+    // <Router >
+    //   <Switch>
+    //     {/* Switch is needed because i have mutiple routes */}
+    //  <Route exact path="/" component={ProductsPage} />
+    //  <Route exact path="/index.html" component={IndexPage} />
+    //  <Route exact path="/products.html" component={ProductsPage} />
+    //  {/* <Route exact path="/contact.html" component={ContactPage} /> */}
+    //  </Switch>
+    // </Router>
   );
   }
 }
